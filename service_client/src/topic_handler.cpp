255f3a4dc044bcfcec769a4d0d341ab420dab102
Илья Смолин <70722225+Ilya-Smolin@users.noreply.github.com>
2021-11-16 18:29:32 +0300

    Add files via upload

--- service_client/src/topic_handler.cpp
+++ service_client/src/topic_handler.cpp
@@ -9,25 +9,30 @@ using namespace std;
 
 void topic_handle (const std_msgs::Float32MultiArray::ConstPtr& topic_array)
 {
-  if ((sizeof (topic_array->data)) / (sizeof (float)) == 2)
+  int number_of_elements = 0;
+  for(std::vector<float>::const_iterator it = topic_array->data.begin();
+  it != topic_array->data.end(); ++it)
+    number_of_elements++;
+  if (number_of_elements == 0)
   {
-    ROS_INFO ("X1 = %f, X2 = %f",
-    topic_array->data[0],
-    topic_array->data[1]);
+    ROS_INFO("No real roots");
     return;
   }
-  if ((sizeof (topic_array->data)) / (sizeof (float)) == 1)
+  if (number_of_elements == 1)
   {
     ROS_INFO("X = %f", topic_array->data[0]);
     return;
   }
-  ROS_INFO("No real roots");
+  ROS_INFO ("X1 = %f, X2 = %f",
+  topic_array->data[0],
+  topic_array->data[1]);
   return;
 }
 
 
 int main(int argc, char **argv)
 {
+  ROS_INFO ("RDY");
   ros::init(argc, argv, "topic_handler");
   ros::NodeHandle node;
   ros::Subscriber subscriber = node.subscribe("topic_reply", 10, topic_handle);
